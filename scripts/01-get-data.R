385a592891cb3d0bedf0e30d9a246ef21db87e28
Kevin Chen <kevchen@berkeley.edu>
2020-11-17 17:08:50 -0800

    Setting up simulation

--- scripts/01-get-data.R
+++ scripts/01-get-data.R
@@ -6,7 +6,7 @@
 library(here)
 source(here::here("scripts", "00-preliminaries.R"))
 
-# Data
+# Data ####
 frm <- read.csv(here::here("data", "frmgham2.csv"))
 frm <- as.data.table(frm)
 names(frm) <- tolower(names(frm))
@@ -15,4 +15,30 @@ str(frm)
 length(unique(frm$randid))
 
 # Does education vary over time?
-frm[,.(educ = n_distinct(educ)), by = .(randid)]$x %>% apply(2, table)
+lapply(c("educ", "cigpday", "bmi", "sex"), function(x) {
+	frm[,.(x = n_distinct(get(x))), by = .(randid)]$x %>% table
+})
+
+# Remove prevalent cases
+prevalent.who <- unique(frm[prevstrk == 1 & period == 1, randid])
+frm <- frm[!randid %in% prevalent.who, ]
+
+# Treatments
+frm[, `:=`(totchol = ifelse(totchol < 125 | totchol > 200, 1, 0))]
+frm
+
+# Make outcomes time-varying
+frm[,`:=`(endtime = shift(time, -1)), by = .(randid)]
+frm[is.na(endtime), endtime := 8766]
+frm[timestrk > endtime, stroke := 0]
+frm[timestrk > endtime, stroke := 1]
+frm[timeap > endtime | stroke == 1, cens := 0]
+frm[timeap <= endtime, cens := 1]
+frm[timechd > endtime, anychd := 0]
+frm[timehyp > endtime, hyperten := 0]
+
+frm.wide <- dcast(
+	frm, randid ~ period,
+	value.var = c("cigpday", "bmi", "anychd", "hyperten", "cens", "totchol", "stroke"))
+
+frm.wide <- merge(frm.wide, frm[,.(educ = educ[1], sex = sex[1]), by = .(randid)], on = "randid")
---
+++ scripts/02-simulation.R
@@ -0,0 +1,65 @@
+# Data preparation and exploration ##################################
+# PH 252E: Group Project
+# October 15, 2020
+
+# Preliminaries (packages and such)
+library(here)
+source(here::here("scripts", "00-preliminaries.R"))
+# Get data
+source(here::here("scripts", "01-get-data.R"))
+setDT(frm)
+frm[,`:=`(
+	totchol = totchol <= 200 & totchol >= 125)]
+
+# Exploratory graphing
+frm %>% ggplot(
+	aes(x = bmi)
+) + geom_histogram(aes(y = ..density..)) +
+	geom_function(fun = function(x) {
+		dgamma(x, 47, 1.85)
+	}, color = "red") +
+	facet_wrap(. ~ period) +
+	mytheme
+
+frm %>% ggplot(
+	aes(x = cigpday)
+) + geom_histogram(aes(y = ..density..)) +
+	geom_function(fun = function(x) {
+		dnorm(x, 20, 10)
+	}, color = "red") +
+	facet_wrap(. ~ period) +
+	mytheme
+
+n <- nrow(frm.wide)
+
+# Exogenous nodes ####
+for (k in 1:3) {
+	sapply(paste("U", c("chd", "hyp", "cens", "A", "Y"),
+			k, sep = "_"), function(x) {
+				assign(x, runif(n))
+			})
+
+	assign(paste0("U_", "cigpday", "_", k), rnorm(n, 20, 5))
+	assign(paste0("U_", "bmi", "_", k), rgamma(n, 40, 1.5))
+}
+
+# Endogenous nodes ####
+# Education
+educ.table <- table(frm[,.(educ = educ[1]), by = .(randid)]$educ)
+educ <- cut(runif(n),
+				 		c(0, cumsum(educ.table/sum(educ.table))),
+				 		1:4,
+				 		include.lowest = T)
+sex <- ifelse(runif(n) > 0.55, 0, 1)
+
+# Fit models to find parameters for simulation
+for (k in 1:3) {
+	assign(paste0("mod", "chd", "_", k),
+				 glm())
+	assign(paste0("hyp", "_", k), rgamma(n, 40, 1.5))
+	assign(paste0("cens", "_", k), rnorm(n, 20, 5))
+	assign(paste0("A", "_", k), rgamma(n, 40, 1.5))
+	assign(paste0("Y", "_", k), rnorm(n, 20, 5))
+	assign(paste0("cigpday", "_", k), rnorm(n, 20, 5))
+	assign(paste0("bmi", "_", k), rgamma(n, 40, 1.5))
+}
